5cdce81817fddc835daec3387a134fbcc821ef12
Nurrohmann <nurrohmanmarduki@gmail.com>
2023-12-21 17:00:11 +0700

    feat: add variable to store pathname

--- src/app/[slug]/page.tsx
+++ src/app/[slug]/page.tsx
@@ -1,13 +1,28 @@
 "use client";
+import { useState, useEffect } from "react";
 import { usePathname } from "next/navigation";
+import { searchPokemon } from "../api/apiCall";
 
 export default function Page() {
+  const [pokeName, setPokename] = useState({});
   const pathName = usePathname();
+  const pokePath = pathName.split("/")[1];
 
-  console.log(pathName.split("/")[1]);
+  useEffect(() => {
+    fetchPokemon();
+  }, [pokePath]);
+
+  const fetchPokemon = async () => {
+    try {
+      const data = await searchPokemon(pokePath);
+      setPokename(data);
+    } catch (error) {}
+  };
+
+  console.log(pokeName);
   return (
     <div>
-      <h1>{pathName}</h1>
+      <h1>{pokeName.name}</h1>
     </div>
   );
 }
