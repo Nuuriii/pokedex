613b0d8667ba1fbbb0aefd83f136cc001d736ae0
Nurrohmann <nurrohmanmarduki@gmail.com>
2023-12-23 14:45:27 +0700

    refactor: remove unused code

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,17 @@
 "use client";
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import Provider from "./provider";
 import Header from "./container/header";
 import PokemonList from "./container/home";
 
 export default function Home() {
   const [search, setSearch] = useState("");
-  // console.log(search);
-  console.log(search);
+
   return (
     <Provider>
       <main className="bg-[#DC0A2D] dark:bg-neutral-950 flex min-h-screen flex-col items-center py-[2rem] px-[0.5rem]">
         <Header onSearch={(str: string) => setSearch(str)} />
         <PokemonList propsName={search} />
-        {/* <LayoutComponent /> */}
       </main>
     </Provider>
   );
