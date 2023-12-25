dd908b8f8ab53a3e6f8f57105bc504b58365de24
Nurrohmann <nurrohmanmarduki@gmail.com>
2023-12-25 20:02:40 +0700

    feat: add style for centering element

--- src/app/container/header.tsx
+++ src/app/container/header.tsx
@@ -16,7 +16,7 @@ export default function Header({ onSearch }: SearchProps) {
   };
 
   return (
-    <div className="w-full px-[1rem] mb-[2rem]">
+    <div className="w-full sm:w-[30rem] px-[1rem] mb-[2rem]">
       <TitleIcon />
       <div className="mt-[1rem] flex items-center justify-between">
         <SearchUi onSearch={(str: string) => setSearch(str)} />
