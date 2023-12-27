e841e9277da866e983a011d94a50599c0ccba403
Nurrohmann <nurrohmanmarduki@gmail.com>
2023-12-28 03:20:34 +0700

    feat: add remove input text

--- src/app/commons/element/searchUi.tsx
+++ src/app/commons/element/searchUi.tsx
@@ -1,16 +1,14 @@
-"use client";
-import { useState, useEffect } from "react";
-import { SearchProps } from "../constant/interface";
-import Image from "next/image";
-import Search from "@/app/assets/search.svg";
-import { FaSearch } from "react-icons/fa";
-import { IoSearchOutline } from "react-icons/io5";
-import Close from "@/app/assets/close.svg";
-
-export let searchValue = "";
+'use client';
+import { useState, useEffect } from 'react';
+import { SearchProps } from '../constant/interface';
+import Image from 'next/image';
+import { IoSearchOutline } from 'react-icons/io5';
+import Close from '@/app/assets/close.svg';
+export let searchValue = '';
+import { CgClose } from 'react-icons/cg';
 
 export default function SearchUi({ onSearch }: SearchProps) {
-  const [search, setSearch] = useState("");
+  const [search, setSearch] = useState('');
 
   useEffect(() => {
     const handleSearch = async () => {
@@ -20,9 +18,17 @@ export default function SearchUi({ onSearch }: SearchProps) {
     handleSearch();
   }, [search, onSearch]);
 
+  const deleteSearching = () => {
+    setSearch('');
+  };
+
   //console.log(search);
   return (
-    <div className="w-[85%] bg-white flex items-center rounded-[3rem] h-[2.5rem] px-[1rem]">
+    <div
+      className={`w-[85%] bg-white flex items-center rounded-[3rem] h-[2.5rem] px-[1rem] ${
+        search === '' ? 'shadow-[_0px_1px_3px_1px_rgba(0,0,0,0.25)_inset]' : ''
+      }`}
+    >
       <label htmlFor="search">
         <IoSearchOutline
           className="text-[#DC0A2D] dark:text-neutral-800 mr-[0.5rem]"
@@ -30,14 +36,21 @@ export default function SearchUi({ onSearch }: SearchProps) {
         />
       </label>
       <input
-        className="w-[70%] text-[#666666] bg-inherit mr-[0.5rem]"
+        className="w-[70%] text-[#666666] bg-inherit mr-[0.5rem] focus:outline-none"
         type="text"
         value={search}
         onChange={(e) => setSearch(e.target.value)}
         placeholder="Search"
         id="search"
       />
-      <Image className="hidden h-[1.5rem] w-auto" src={Close} alt="" />
+      <button
+        onClick={deleteSearching}
+        className={`${
+          search === '' ? 'hidden' : 'flex'
+        } justify-center items-center h-[2rem] w-[2rem] rounded-full hover:bg-red-100 dark:hover:bg-neutral-200`}
+      >
+        <CgClose size={20} className={`text-[#DC0A2D] dark:text-neutral-800`} />
+      </button>
     </div>
   );
 }
