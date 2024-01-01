6cbac9b9215c548973560ea758fc8310e411b296
Nurrohmann <nurrohmanmarduki@gmail.com>
2024-01-01 16:58:22 +0700

    feat: add to top button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@ import { useState } from 'react';
 import Provider from './provider';
 import Header from '@/app/container/header';
 import PokemonList from '@/app/container/content';
+import BackToTop from './commons/element/backToTop';
 
 export default function Home() {
   const [search, setSearch] = useState('');
@@ -12,6 +13,7 @@ export default function Home() {
       <main className="bg-[#DC0A2D] dark:bg-[#330109] flex min-h-screen flex-col items-center py-[2rem] px-[0.5rem]">
         <Header onSearch={(str: string) => setSearch(str)} />
         <PokemonList propsName={search} />
+        <BackToTop />
       </main>
     </Provider>
   );
