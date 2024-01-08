36aa77de04452d9fb345252786a8eccf2fd887eb
Nurrohmann <nurrohmanmarduki@gmail.com>
2024-01-08 08:54:28 +0700

    feat: setup next pwa

--- next.config.js
+++ next.config.js
@@ -1,13 +1,25 @@
 /** @type {import('next').NextConfig} */
 //const nextConfig = {};
 
-module.exports = {
+const withPWA = require('@ducanh2912/next-pwa').default({
+  dest: 'public',
+  cacheOnFrontEndNav: true,
+  aggressiveFrontEndNavCaching: true,
+  reloadOnOnline: true,
+  swcMinify: true,
+  disable: false,
+  workboxOptions: {
+    disableDevLogs: true,
+  },
+});
+
+module.exports = withPWA({
   images: {
     remotePatterns: [
       {
-        protocol: "https",
-        hostname: "raw.githubusercontent.com",
+        protocol: 'https',
+        hostname: 'raw.githubusercontent.com',
       },
     ],
   },
-};
+});
