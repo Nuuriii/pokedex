8a329935870a7d0c379b88e069c1154e8050e337
Nurrohmann <nurrohmanmarduki@gmail.com>
2024-02-16 17:04:46 +0700

    feat: add redux provider

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,24 @@
 'use client';
 import { useState } from 'react';
-import Provider from './provider';
+import NextThemeProvider from './provider';
 import Header from '@/app/container/header';
 import PokemonList from '@/app/container/content';
 import BackToTop from './commons/element/backToTop';
+import { store } from './store/store';
+import { Provider } from 'react-redux';
 
 export default function Home() {
   const [search, setSearch] = useState('');
 
   return (
-    <Provider>
-      <main className="bg-[#DC0A2D] dark:bg-[#330109] flex min-h-screen flex-col items-center py-[2rem] px-[0.5rem]">
-        <Header onSearch={(str: string) => setSearch(str)} />
-        <PokemonList propsName={search} />
-        <BackToTop />
-      </main>
+    <Provider store={store}>
+      <NextThemeProvider>
+        <main className="bg-[#DC0A2D] dark:bg-[#330109] flex min-h-screen flex-col items-center py-[2rem] px-[0.5rem]">
+          <Header onSearch={(str: string) => setSearch(str)} />
+          <PokemonList propsName={search} />
+          <BackToTop />
+        </main>
+      </NextThemeProvider>
     </Provider>
   );
 }
